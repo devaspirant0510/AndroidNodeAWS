ecb904490d0b89fca09dbc3eda2d8ed3122d4301
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-05-25 23:47:10 +0900

    volley network 통신, json파싱

--- client/app/src/main/java/com/example/androidserver/MainActivity.java
+++ client/app/src/main/java/com/example/androidserver/MainActivity.java
@@ -1,14 +1,109 @@
 package com.example.androidserver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private String buffer;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text_view);
+        String url =  "http://ec2-3-36-106-46.ap-northeast-2.compute.amazonaws.com:3001/";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                        try {
+                            JSONObject json = new JSONObject(response);
+
+                            Log.e("asdf", "onResponse: "+json.getString("0"));
+                            JSONObject json2 = new JSONObject(json.getString("0"));
+                            Log.e("asdf",json2.getString("name"));
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText(error.getMessage());
+
+                    }
+                }
+
+        );
+        queue.add(request);
+
+       // String url = "http://ec2-3-36-106-46.ap-northeast-2.compute.amazonaws.com:3001/";
+//        String url = "www.google.co.kr";
+//        buffer = "";
+//        StringRequest request = new StringRequest(
+//                Request.Method.GET,
+//                url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        textView.setText(response);
+//
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        textView.setText("error");
+//
+//                    }
+//                }
+//        );
+//        queue.add(request);
+
+    }
+    private boolean permissionNetwork(){
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            if (checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
+
+
+                return false;
+            }
+        }else{
+            Toast.makeText(this, "External Storage Permission is Grant", Toast.LENGTH_SHORT).show();
+            return true;
+        }
     }
 }
