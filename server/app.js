70e2c236d3d07173ebdcd8787faaa6b7d0ff1473
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-06-03 11:22:20 +0900

    post 요청 구현 bodyparser 에서 헤더값 가져옴

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-
+const bodyParser =require("body-parser");
 
 const app = express();
 
@@ -8,27 +8,28 @@ let data = fs.readFileSync(`${__dirname}/movies.json`);
 data = JSON.parse(data);
 //let json_data = JSON.stringify(data);
 
+function addJsonData(id,title,director,year,synopsis){
+    return {id:id,
+            title:title,
+            director:director,
+            year:year,
+            synopsis:synopsis};
+}
+
+app.use("/",express.static("html"));
 app.use("/game",express.static("game"));
+app.use(bodyParser.urlencoded({extended:true}));
 
-
-app.get("/",(req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
-    res.end("모바일 API 서버");
+app.get("/s",(req, res) => {
+    //res.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
+    // fs.readFile("html/index.html",(err,data)=>{
+    //     res.end(data);
+    // });
+    console.log("d");
+    console.log(req.hostname);
+    res.redirect("http://www.naver.com");
 });
 
-app.get("/movies",(req, res) => {
-    const movie_list = [];
-    res.writeHead(200,{"Content-Type":"application/json;charset=utf-8"});
-
-    data.forEach(value => {
-        movie_list.push({id:value['id'],title:value['title']});
-    });
-
-    const data2obj = {data:movie_list};
-    
-    const data2json = JSON.stringify(data2obj);
-    res.end(data2json);
-});
 
 app.get("/game",((req, res) => {
     console.log(__dirname)
@@ -46,7 +47,35 @@ app.get("/movies/:id",(req, res) => {
     res.end(index_data);
 });
 
+app.post("/movies",((req, res,next) => {
+    const id = data.length;
+    const title = req.body.title;
+    const director = req.body.director;
+    const year = req.body.year;
+    const synopsis = req.body.synopsis;
+    const jsonBody = addJsonData(id,title,director,year,synopsis);
+    data.push(jsonBody);
+    console.log(typeof (data));
+    fs.writeFileSync(`${__dirname}/movies.json`,JSON.stringify(data));
+    //res.writeHead(200,{"Content-Type":"text/plain;charset=utf-8"});
+    //res.redirect("https://www.google.co.kr");
+    // res.end();
+    res.redirect(`/movies`);
 
+}))
+app.get("/movies",(req, res) => {
+    const movie_list = [];
+    res.writeHead(200,{"Content-Type":"application/json;charset=utf-8"});
+
+    data.forEach(value => {
+        movie_list.push({id:value['id'],title:value['title']});
+    });
+
+    const data2obj = {data:movie_list};
+
+    const data2json = JSON.stringify(data2obj);
+    res.end(data2json);
+});
 
 app.listen(3001,()=>{
     console.log("127.0.0.1:3001");
