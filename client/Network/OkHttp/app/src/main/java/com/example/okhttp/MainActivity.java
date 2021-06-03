aba9d74be6edbccecfeb40a5f8dccaddc773a4c7
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-06-03 12:38:20 +0900

    ok Http post 요청

--- client/Network/OkHttp/app/src/main/java/com/example/okhttp/MainActivity.java
+++ client/Network/OkHttp/app/src/main/java/com/example/okhttp/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Handler;
 import android.util.JsonReader;
 import android.util.JsonToken;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,24 +23,40 @@ import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements InfoDialog.BottomSheetCallback {
     private static final String TAG = "MainActivity";
     private RecyclerView recyclerView;
     private RcAdapter adapter;
+    private Button requestPost;
+    private OkHttpClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        OkHttpClient client = new OkHttpClient();
+        client = new OkHttpClient();
         recyclerView = findViewById(R.id.json_list);
+        requestPost = findViewById(R.id.request_post);
+
+
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         adapter = new RcAdapter();
         recyclerView.setAdapter(adapter);
 
+        requestPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InfoDialog dialog = new InfoDialog(MainActivity.this);
+                dialog.setBottomSheetCallback(MainActivity.this);
+                dialog.show();
+
+            }
+        });
 
         FormBody formBody = new FormBody.Builder().build();
 
@@ -86,6 +104,36 @@ public class MainActivity extends AppCompatActivity {
 
             }
             response.close();
+        }
+    };
+
+    @Override
+    public void setOnData(String title, String director, int year, String synopsis) {
+        RequestBody body = new FormBody.Builder()
+                .add("title",title)
+                .add("director",director)
+                .add("year",String.valueOf(year))
+                .add("synopsis",synopsis).build();
+        Request request = new Request.Builder()
+
+                .url("http://ec2-3-36-106-46.ap-northeast-2.compute.amazonaws.com:3001/movies")
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(updateUserCallBack);
+    }
+    private Callback updateUserCallBack = new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+            String body;
+            if (response.body() != null) {
+                body = response.body().toString();
+                Log.e(TAG, "onResponse: "+body );
+            }
         }
     };
 }
