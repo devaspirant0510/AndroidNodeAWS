67efe42716a76db7532a8b0cf7553c7d4bd5baf5
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-06-05 14:04:11 +0900

    새로고침

--- server/game/index.js
+++ server/game/index.js
@@ -13,12 +13,12 @@ let flag = true;
 let card2count = [];
 let openCardList = [];
 //
-// const retry_btn = document.querySelector("#retry-btn");
+const retry_btn = document.querySelector("#retry-btn");
 // const retry_btn = document.querySelector("#ranking-btn");
 
-// retry_btn.addEventListener("click",(ev)=>{
-//     location.reload();
-// })
+retry_btn.addEventListener("click",(ev)=>{
+    location.reload();
+})
 function shuffle(list){
     let color_list = [];
     for (let i = 0; 0 < list.length; i++) {
