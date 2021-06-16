1372225b59a261f3798bd1df39acdad39ee47560
root <root@ip-172-31-10-145.ap-northeast-2.compute.internal>
2021-06-16 01:03:18 +0000

    delete,put 요청 구현

--- server/app.js
+++ server/app.js
@@ -1,83 +1,94 @@
 const express = require("express");
 const fs = require("fs");
-const bodyParser =require("body-parser");
 
 const app = express();
 
 let data = fs.readFileSync(`${__dirname}/movies.json`);
 data = JSON.parse(data);
+
 //let json_data = JSON.stringify(data);
 
-function addJsonData(id,title,director,year,synopsis){
-    return {id:id,
-            title:title,
-            director:director,
-            year:year,
-            synopsis:synopsis};
+function addJsonData(id, title, director, year, synopsis) {
+    return {
+        id: id,
+        title: title,
+        director: director,
+        year: year,
+        synopsis: synopsis
+    };
 }
 
 // app.use("/",express.static("html"));
-app.use("/game",express.static("game"));
-app.use("/a",express.static("views"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use("/game", express.static("game"));
+app.use("/a", express.static("views"));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
-app.get("/",(req, res) => {
-    fs.readFile(__dirname+"\\html\\index.html",(err, data1) => {
-        console.log(data1);
-        res.end(data1);
-    })
-});
-app.get("/mypage",(req,res)=>{
-    //res.writeHead(200,{"Content-Type":"text/html"});
-
-    fs.readFile("./html/index.html",(err, data1) => {
-        res.end(data1);
-    });
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/html/index.html");
 });
 
-app.get("/game",((req, res) => {
-    console.log(__dirname)
-    fs.readFile("index.game",(err,data)=>{
-        console.log(data);
-        res.end(data);
-    });
+app.get("/game", ((req, res) => {
+    res.sendFile(__dirname+"/game/index.html");
 }))
 
-app.get("/movies/:id",(req, res) => {
-    const id = req.params.id;
-    let index_data = data[id];
-    index_data = JSON.stringify(index_data);
-    res.writeHead(200,{"Content-Type":"application/json;charset=utf-8"});
-    res.end(index_data);
-});
 
-app.post("/movies",((req, res,next) => {
+app.post("/movies", ((req, res, next) => {
     const id = data.length;
     const title = req.body.title;
     const director = req.body.director;
-    const year = req.body.year;
+    const year = Number(req.body.year);
     const synopsis = req.body.synopsis;
-    const jsonBody = addJsonData(id,title,director,year,synopsis);
+    const jsonBody = addJsonData(id, title, director, year, synopsis);
     data.push(jsonBody);
-    console.log(typeof (data));
-    fs.writeFileSync(`${__dirname}/movies.json`,JSON.stringify(data));
-    res.redirect(`/movies`);
+    fs.writeFileSync(`${__dirname}/movies.json`, JSON.stringify(data));
+    res.json(jsonBody);
 
 }))
-app.get("/movies",(req, res) => {
+app.get("/movies", (req, res) => {
     const movie_list = [];
-    res.writeHead(200,{"Content-Type":"application/json;charset=utf-8"});
 
     data.forEach(value => {
-        movie_list.push({id:value['id'],title:value['title']});
+        movie_list.push({id: value['id'], title: value['title']});
     });
 
-    const data2obj = {data:movie_list};
+    const data2obj = {data: movie_list};
 
-    const data2json = JSON.stringify(data2obj);
-    res.end(data2json);
+    res.json(data2obj);
 });
 
-app.listen(3001,()=>{
+app.get("/movies/:id", (req, res) => {
+    const id = req.params.id;
+    let index_data = data[id];
+    res.json(index_data);
+});
+
+app.put("/movies/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const title = req.body.title;
+    console.log(title)
+    const director = req.body.director;
+    const year = req.body.year;
+    const synopsis = req.body.synopsis;
+    const jsonBody = addJsonData(id, title, director, year, synopsis);
+    data[id] = jsonBody;
+    fs.writeFileSync(`${__dirname}/movies.json`, JSON.stringify(data));
+    res.json(jsonBody);
+
+
+});
+app.delete("/movies/:id", (req, res) => {
+    const id = req.params.id;
+    const deleteJson = {index:id,data:data[id]}
+    console.log(deleteJson);
+    data.splice(id, 1);
+    console.log(data)
+    fs.writeFileSync(`${__dirname}/movies.json`, JSON.stringify(data));
+    res.json(deleteJson);
+
+});
+
+app.listen(3001, () => {
     console.log("127.0.0.1:3001");
 })
